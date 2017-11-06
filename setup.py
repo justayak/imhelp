e1a5125ddaa2f727026851e419a3169733693071
julian.tanke <julian.tanke@fit.fraunhofer.de>
2017-11-06 10:20:21 +0100

    add numpy dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
+setup_req = ['numpy']
+install_req = ['numpy']
+
 setup(
     name = 'imhelp',
     packages = ['imhelp'],
+    install_requires=install_req,
+    setup_requires=setup_req,
     version = '0.0.1',
     description = 'General Image Helper Functions',
-    author = 'Julian Tanke'
+    author = 'Julian Tanke',
+    url='https://github.com/justayak/imhelp'
 )
