efcf573aa9c63e539b99a757ee069b491fdadff2
Julian Tanke <jutanke@googlemail.com>
2017-11-06 10:43:14 +0100

    update name

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup_req = ['numpy']
 install_req = ['numpy']
 
 setup(
-    name = 'imhelp',
-    packages = ['imhelp'],
+    name = 'pppr',
+    packages = ['pppr'],
     install_requires=install_req,
     setup_requires=setup_req,
     version = '0.0.1',
     description = 'General Image Helper Functions',
     author = 'Julian Tanke',
-    url='https://github.com/justayak/imhelp'
+    url='https://github.com/justayak/pppr'
 )
