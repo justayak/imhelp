490de5337b5594a214161adc83202d8f82a29b3d
root <jutanke@googlemail.com>
2017-12-06 14:54:52 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from distutils.core import setup
 
-setup_req = ['numpy']
-install_req = ['numpy']
-
 setup(
     name = 'pppr',
     packages = ['pppr'],
-    install_requires=install_req,
-    setup_requires=setup_req,
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'General Image Helper Functions',
     author = 'Julian Tanke',
     url='https://github.com/justayak/pppr'
